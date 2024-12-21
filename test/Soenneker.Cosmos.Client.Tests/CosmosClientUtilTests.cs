77edbdcb31062cc080c192e41d3ee22feb4cade4
Jake Soenneker <jake@soenneker.com>
2024-12-20 20:02:33 -0600

    Tests

---
+++ test/Soenneker.Cosmos.Client.Tests/CosmosClientUtilTests.cs
@@ -0,0 +1,23 @@
+using Soenneker.Cosmos.Client.Abstract;
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+
+namespace Soenneker.Cosmos.Client.Tests;
+
+[Collection("Collection")]
+public class CosmosClientUtilTests : FixturedUnitTest
+{
+    private readonly ICosmosClientUtil _util;
+
+    public CosmosClientUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+        _util = Resolve<ICosmosClientUtil>(true);
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
