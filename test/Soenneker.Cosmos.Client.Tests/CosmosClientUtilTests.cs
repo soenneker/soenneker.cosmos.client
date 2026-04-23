0ed6ef00b883bef435d07b3bea39a9a5e721c698
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-22 19:50:09 -0500

    Various fixes

--- test/Soenneker.Cosmos.Client.Tests/CosmosClientUtilTests.cs
+++ test/Soenneker.Cosmos.Client.Tests/CosmosClientUtilTests.cs
@@ -24,8 +24,8 @@ public class CosmosClientUtilTests : HostedUnitTest
     [Test]
     public async Task Get_TwoClients_AreNotNull()
     {
-        CosmosClient client1 = await _util.Get("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", CancellationToken);
-        CosmosClient client2 = await _util.Get("https://localhost:8080", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", CancellationToken);
+        CosmosClient client1 = await _util.Get("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", System.Threading.CancellationToken.None);
+        CosmosClient client2 = await _util.Get("https://localhost:8080", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", System.Threading.CancellationToken.None);
 
         client1.Should()
                .NotBeNull();
